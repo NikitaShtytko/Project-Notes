3f9d285554052b98e390fb21685ee8ecf6dd46d8
BlaZe <shtytko.nikita@gmail.com>
2020-06-30 15:34:18 +0300

    End to end cases

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,12 +6,22 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
 import { LeftMenuComponent } from './component/left-menu/left-menu.component';
 import {FormsModule} from "@angular/forms";
+import { TopicComponent } from './component/end-to-end/topic/topic.component';
+import {Routes} from "@angular/router";
+import { NotesComponent } from './component/end-to-end/notes/notes.component';
+
+const appRoutes: Routes = [
+  {path: 'topic', component: NotesComponent},
+  {path: 'note', component: TopicComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LeftMenuComponent
+    LeftMenuComponent,
+    TopicComponent,
+    NotesComponent
   ],
   imports: [
     BrowserModule,
