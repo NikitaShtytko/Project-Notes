389dbadf05530c73693655eb198b59a7ff079fb0
BlaZe <shtytko.nikita@gmail.com>
2020-09-06 02:34:51 +0300

    fix for note by topic page

--- frontend/src/app/component/end-to-end/note-by-topic/note-by-topic.component.ts
+++ frontend/src/app/component/end-to-end/note-by-topic/note-by-topic.component.ts
@@ -1,8 +1,9 @@
-import {ChangeDetectionStrategy, Component, Input, OnChanges, OnInit, SimpleChange, SimpleChanges} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Note} from "../../../entity/note";
 import {NoteService} from "../../../service/note/note.service";
-import {Subscription} from "rxjs";
-import {ActivatedRoute, Router} from '@angular/router';
+import {Subject, Subscription} from "rxjs";
+import {ActivatedRoute, Router, Params} from '@angular/router';
+import {takeUntil} from "rxjs/operators";
 
 @Component({
   selector: 'app-note-by-topic',
@@ -10,9 +11,10 @@ import {ActivatedRoute, Router} from '@angular/router';
   styleUrls: ['./note-by-topic.component.css'],
   // changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class NoteByTopicComponent implements OnInit{
+export class NoteByTopicComponent implements OnDestroy{
 
   // @Input() topic: String;
+  private destroy$ = new Subject<undefined>();
   public topic: String;
   public note: Note[];
   public subscriptions: Subscription[] = [];
@@ -22,15 +24,29 @@ export class NoteByTopicComponent implements OnInit{
               private activateRoute: ActivatedRoute,
               ){
     this.topic = activateRoute.snapshot.params.topic
+    this.activateRoute.params.pipe(
+      takeUntil(this.destroy$)
+    ).subscribe((params: Params) => {
+      this.topic = params.topic;
+      this.subscriptions.push(this.noteService.getNotesByTopic(this.topic).subscribe(response => {
+        this.note = response;
+        console.log("message")
+      }));
+    });
   }
 
-  ngOnInit(): void {
-    console.log(this.topic);
-    this.subscriptions.push(this.noteService.getNotesByTopic(this.topic).subscribe(response => {
-      this.note = response;
-      console.log("message")
-    }));
+  public ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
+
+  // ngOnInit(): void {
+  //   console.log(this.topic);
+  //   this.subscriptions.push(this.noteService.getNotesByTopic(this.topic).subscribe(response => {
+  //     this.note = response;
+  //     console.log("message")
+  //   }));
+  // }
 
   equip(header: string, topic: string): string{
     if (header != null){
